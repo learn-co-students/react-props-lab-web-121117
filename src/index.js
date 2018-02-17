7b15ba0f2366d9831628144f635788e5256da0fc
github username <github email address>
2018-02-17 16:03:29 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Spaceship from './components/Spaceship';
+import Spaceship from "./components/Spaceship";
 
 ReactDOM.render(
   <Spaceship name="Millennium Falcon" />,
-  document.getElementById('global')
+  document.getElementById("global")
 );
